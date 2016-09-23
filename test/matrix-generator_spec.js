8403d8afb3a369098989c36727263b7d4880d47f
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-23 10:15:44 -1000

    completed matrix generator

--- test/matrix-generator_spec.js
+++ test/matrix-generator_spec.js
@@ -8,9 +8,5 @@ describe('Matrix Generator', () => {
     expect(matrixGenerator).to.exist;
     expect(matrixGenerator).to.be.a('function');
   });
-    describe('create object', () => {
-      it('should create an object', () => {
-        expect(matrixGenerator(3,3).to.equal([]));
-      });
-    });
+
 });
